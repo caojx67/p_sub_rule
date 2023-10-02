578db66400cda1683996e8e10d3c292bc2100169
caojx67 <142964809+caojx67@users.noreply.github.com>
2023-10-02 23:00:07 +0800

    Update ssr_conver.js

--- js/ssr_conver.js
+++ js/ssr_conver.js
@@ -1,12 +1,10 @@
 var requestURL = $request.url;
 console.log('当前请求的URL:'+ requestURL);
 
-let body = $response.body;
+let bodyStr = $response.body;
 
-// 在这里对响应的 body 进行修改，例如将 "original_text" 替换为 "modified_text"
-console.log('当前请求的URL body :'+ body);
+console.log('当前请求的URL body :'+ bodyStr);
 
-body = '1234567'
+bodyStr = '123456';
 
-
-$done({ body: body });
+$done({ body: bodyStr });
