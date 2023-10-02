b3e4e9e91b317e1ebe970494694f0fc3e207569a
caojx67 <142964809+caojx67@users.noreply.github.com>
2023-10-02 23:12:45 +0800

    Update ssr_conver.js

--- js/ssr_conver.js
+++ js/ssr_conver.js
@@ -1,14 +1,23 @@
-/**
+var zlib = require('zlib');
 
 var requestURL = $request.url;
-console.log('当前请求的URL:'+ requestURL);
+console.log('当前请求的URL: ' + requestURL);
 
-let bodyStr = $response.body;
+var body = $response.body;
 
-console.log('当前请求的URL body :'+ bodyStr);
+// 解压缩 Gzip 压缩的响应主体
+if ($response.headers['Content-Encoding'] == 'gzip') {
+  body = zlib.gunzipSync(body);
+  console.log('解压缩后的响应主体: ' + body);
+}
 
+// 在这里对响应的 body 进行修改，例如将 "original_text" 替换为 "modified_text"
+// body = body.toString('utf-8').replace('original_text', 'modified_text');
+body = "12345678";
 
-***/
-let bodyStr = '123456';
+// 重新压缩响应主体为 Gzip 格式
+if ($response.headers['Content-Encoding'] == 'gzip') {
+  body = zlib.gzipSync(body);
+}
 
-$done({ body: bodyStr });
+$done({ body: body });
