3610bdba6440a684364b5774cd61a7d24b70243c
caojx67 <142964809+caojx67@users.noreply.github.com>
2023-10-02 22:58:01 +0800

    Update ssr_conver.js

--- js/ssr_conver.js
+++ js/ssr_conver.js
@@ -6,7 +6,7 @@ let body = $response.body;
 // 在这里对响应的 body 进行修改，例如将 "original_text" 替换为 "modified_text"
 console.log('当前请求的URL body :'+ body);
 
-body = JSON.stringify({'a':'2'});
+body = '1234567'
 
 
 $done({ body: body });
